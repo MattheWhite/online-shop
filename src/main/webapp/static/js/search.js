be4bb57649bd0058fe92b2c7558e45a0530651cf
Maarsupilami <vajger.mark@outlook.hu>
2022-11-16 18:16:16 +0100

    Refactor filter implementation

--- src/main/webapp/static/js/search.js
+++ src/main/webapp/static/js/search.js
@@ -1,25 +1,33 @@
-function filterDrinks () {
-    const products = document.querySelectorAll(".col");
-    const filterButton = document.querySelector("#filter-button");
-    const filterAlcoholic = document.querySelector("#filter-alcoholic");
-    let filterExpression = "";
+function setProducts(products, filterExpression) {
+    for (const product of products) {
+        if (product.dataset.category.toLowerCase() === filterExpression.toLowerCase()) {
+            product.style.display = "inline-block";
+        } else if (filterExpression === "None") {
+            product.style.display = "inline-block";
+        } else {
+            product.style.display = "none";
+        }
+    }
+}
 
+function filterButtonManager(filterButton, filterAlcoholic, filterExpression, products) {
     filterButton.addEventListener('click', function () {
         for (const filterAlcoholicElement of filterAlcoholic) {
             if (filterAlcoholicElement.selected) {
                 filterExpression = filterAlcoholicElement.innerText;
             }
         }
-        for (const product of products) {
-            if (product.dataset.category.toLowerCase() === filterExpression.toLowerCase()) {
-                product.style.display = "inline-block";
-            } else if (filterExpression === "None") {
-                product.style.display = "inline-block";
-            } else {
-                product.style.display = "none";
-            }
-        }
+        setProducts(products, filterExpression);
     })
+}
+
+function filterDrinks() {
+    const products = document.querySelectorAll(".col");
+    const filterButton = document.querySelector("#filter-button");
+    const filterAlcoholic = document.querySelector("#filter-alcoholic");
+    let filterExpression = "";
+
+    filterButtonManager(filterButton, filterAlcoholic, filterExpression, products);
 }
 
 filterDrinks();
