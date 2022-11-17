5464b7ee35017695062d2c665792142bd02ac44d
MattheWhite <fehermatyas97@gmail.com>
2022-11-17 10:12:27 +0100

    Create two constants for the http requests type, delete and post for product handling in cart.

--- src/main/webapp/static/js/cart.js
+++ src/main/webapp/static/js/cart.js
@@ -1,16 +1,25 @@
+const HTTP_POST = "POST";
+const HTTP_DELETE = "DELETE";
+
 function main() {
     let addToCartButtons = document.querySelectorAll(".add-to-cart");
     addToCartButtons.forEach(button => button.addEventListener('click', () => {
         let productId = button.dataset.id;
-        sendProductId(productId);
+        sendProductId(productId, HTTP_POST);
     }))
     let navBar = document.querySelector('#mainNav');
     navBar.classList.add("navbar-shrink");
+
+    let deleteButtons = document.querySelectorAll(".trash-icon");
+    deleteButtons.forEach(button => button.addEventListener('click', () => {
+        let productId = button.dataset.id;
+        sendProductId(productId, HTTP_DELETE);
+    }))
 }
 
-function sendProductId(productId) {
+function sendProductId(productId, requestType) {
     fetch('/cart', {
-        method:"POST",
+        method:requestType,
         headers: {
             "Content-Type":"application/json"
         },
