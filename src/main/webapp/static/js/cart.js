08c730dfb2f9a34d211f8ed1f1e8e4c1a9d85e4c
MattheWhite <fehermatyas97@gmail.com>
2022-11-17 10:41:58 +0100

    Rewrite delete method, select the item first, then add listener to the item's trash icon.
    First send DELETE request to the server /cart route, then remove the node from the page. NOT ASYNC.

--- src/main/webapp/static/js/cart.js
+++ src/main/webapp/static/js/cart.js
@@ -10,11 +10,15 @@ function main() {
     let navBar = document.querySelector('#mainNav');
     navBar.classList.add("navbar-shrink");
 
-    let deleteButtons = document.querySelectorAll(".trash-icon");
-    deleteButtons.forEach(button => button.addEventListener('click', () => {
-        let productId = button.dataset.id;
-        sendProductId(productId, HTTP_DELETE);
-    }))
+    let cartItems = document.querySelectorAll(".cart-item");
+    cartItems.forEach(item => {
+        let productId = item.dataset.id;
+        let trashIcon = item.querySelector(".trash-icon");
+        trashIcon.addEventListener('click', () => {
+            sendProductId(productId, HTTP_DELETE);
+            item.remove();
+        });
+    });
 }
 
 function sendProductId(productId, requestType) {
